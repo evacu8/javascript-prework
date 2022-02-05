fa4fc860ba0288be84d71955a9be3215c94e75f9
Krzysztof Potocki <kpotocki89@gmail.com>
2022-02-05 18:50:15 +0100

    Add result, change playerMove

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ if (randomNumber == 1) {
     computerMove = 'nożyce';
 }
 
-printMessage(`Mój ruch to ${computerMove}`);
+printMessage(`Mój ruch to ${computerMove}.`);
 
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
@@ -25,8 +25,19 @@ if (playerInput == 1) {
     playerMove = 'kamień';
 } else if (playerInput == 2) {
     playerMove = 'papier';
-} else {
+} else if (playerInput == 3){
     playerMove = 'nożyce';
 } 
 
 printMessage(`Twój ruch to ${playerMove}.`);
+
+
+if (computerMove == 'kamień' && playerMove == 'kamień' || computerMove == 'papier' && playerMove == 'papier' || computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis!');
+} else if (computerMove = 'kamień' && playerMove == 'papier' || computerMove == 'papier' && playerMove == 'nożyce' || computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+} else if (playerMove !== 'kamień' && playerMove !== 'papier' && playerMove !== 'nożyce') {
+    printMessage('Niepoprawny ruch! Spróbuj jeszcze raz.');
+} else {
+    printMessage('Przegrywasz!');
+}
